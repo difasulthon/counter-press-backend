492450d8175ce1e12dc508fee2c5c4b7f0116ee7
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-09-25 23:10:59 +0700

    feat: udpate cors setup

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { brand } from "./routes/Brands.route";
 
 const api = new OpenAPIHono({ strict: false });
 
-api.use(ROUTES.BASE, cors());
+api.use("/*", cors());
 api.route(ROUTES.BASE, product);
 api.route(ROUTES.BASE, brand);
 api.notFound((c) => c.json({ message: MESSAGE.ERROR.NOT_FOUND }, 404));
