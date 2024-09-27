06d7f87af5da2d05dd5b2f43bd18c6dfcb20ccee
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2024-09-27 21:32:48 +0700

    feat: add formatter by slugify

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,5 +1,13 @@
-export const formatSlug = (param: string): string => {
-  const result = param.trim().toLowerCase().replace(" ", "-");
+import slugify from "slugify";
+
+export const formatSlug = (param?: string): string => {
+  if (!param) return "";
+
+  const result = slugify(param, {
+    lower: true,
+    strict: true,
+    trim: true,
+  });
 
   return result;
 };
